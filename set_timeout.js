946266913420b5b37776bebe9b7856b583b428f1
srijan <srijandasbiswas7540@gmail.com>
2023-03-28 16:29:05 +0530

    settimeout

---
+++ set_timeout.js
@@ -0,0 +1,21 @@
+// let a = setTimeout(function(){
+//     alert("srijan")
+// },5000)
+// // a -> timerid
+// let b = prompt("Do you want to run the settimeout?")
+// if(b=="no"){
+//     clearTimeout(a)
+// }
+
+// console.log(a)
+
+const sum = (a,b) => {
+    console.log("sum running" + " " + (a+b))
+    return a+b
+}
+// function sum(a,b){
+//     console.log("sum running" + (a+b))
+//     return a+b
+// }
+setTimeout(sum,2000,2,3)
+// setTimeout(function,timer,parameter1,parameter2)
